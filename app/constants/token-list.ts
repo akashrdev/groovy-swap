d09221fd2249878971621fdc6a40973f82235fbb
akashrdev <akashrajan93@gmail.com>
2025-02-22 15:37:19 -0600

    Fix: TokenItem type missing name

--- app/constants/token-list.ts
+++ app/constants/token-list.ts
@@ -6,11 +6,13 @@ export const DEFAULT_TOKEN_LIST: Record<string, TokenItem> = {
     logo: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
     symbol: "USDC",
     decimals: 6,
+    name: "USD Coin",
   },
   SOL: {
     mintAddress: "So11111111111111111111111111111111111111112",
     logo: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
     symbol: "SOL",
     decimals: 9,
+    name: "Solana",
   },
 };
