662d92a7a27c5bc3705be8b7407469fe7f950e99
akashrdev <akashrajan93@gmail.com>
2025-02-22 19:42:50 -0600

    Style: Add backgrounds to wallet connect

--- app/components/wallet-connect/menu/menu-actions.tsx
+++ app/components/wallet-connect/menu/menu-actions.tsx
@@ -6,17 +6,20 @@ export const WalletConnectMenuActions = () => {
   const { disconnect } = useWallet();
   const { setVisible } = useWalletModal();
   return (
-    <>
+    <div className="bg-primary-card px-4 py-3 rounded-xl flex flex-col items-center gap-3 cursor-pointer">
       <DropDownMenu.Item
         onClick={() => setVisible(true)}
-        className="cursor-pointer"
+        className="hover:bg-primary-card-hover py-1 px-2"
       >
         <span>Change Wallet</span>
       </DropDownMenu.Item>
 
-      <DropDownMenu.Item onClick={disconnect} className="cursor-pointer">
+      <DropDownMenu.Item
+        onClick={disconnect}
+        className="hover:bg-primary-card-hover py-1 px-2"
+      >
         <span>Disconnect</span>
       </DropDownMenu.Item>
-    </>
+    </div>
   );
 };
