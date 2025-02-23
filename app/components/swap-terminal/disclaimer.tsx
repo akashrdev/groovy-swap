851292d7c0357594be64597886b38764ba248c39
akashrdev <akashrajan93@gmail.com>
2025-02-22 23:32:59 -0600

    Feat: Add Jupiter Ecosystem List disclaimer

---
+++ app/components/swap-terminal/disclaimer.tsx
@@ -0,0 +1,15 @@
+export const Disclaimer = () => {
+  return (
+    <span className="text-white/60 text-sm text-center">
+      To protect Groovy Swap users, only tokens from the Jupiter Ecosystem List
+      are tradeable. Learn more{" "}
+      <a
+        href="https://www.jupresear.ch/t/get-your-token-a-community-tag-beta/18963"
+        className="text-sm text-primary-btn"
+      >
+        here
+      </a>
+      .
+    </span>
+  );
+};
--- app/components/swap-terminal/index.tsx
+++ app/components/swap-terminal/index.tsx
@@ -4,19 +4,23 @@ import { Divider } from "../common/divider";
 import { TokenSelectSection } from "./token-select/token-select-section";
 import { SwapButton } from "./buttons/swap-button";
 import { SwitchDirectionButton } from "./buttons/switch-direction-button";
+import { Disclaimer } from "./disclaimer";
 
 export const SwapTerminal = () => {
   return (
-    <div className="relative sm:w-[500px] w-full border bg-secondary-dark border-secondary-card rounded-xl min-h-[450px] p-4 gap-4 flex flex-col">
-      <div className="w-full h-full">
-        <TokenSelectSection tokenDirection={TOKEN_DIRECTION.INPUT} />
-        <div className="relative w-full flex justify-center">
-          <Divider className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 mx-auto border-secondary-card" />
-          <SwitchDirectionButton className="z-10 p-2 rounded-full" />
+    <div className="flex flex-col gap-2 sm:w-[500px] w-full">
+      <div className="relative border bg-secondary-dark border-secondary-card rounded-xl min-h-[450px] p-4 gap-4 flex flex-col">
+        <div className="w-full h-full">
+          <TokenSelectSection tokenDirection={TOKEN_DIRECTION.INPUT} />
+          <div className="relative w-full flex justify-center">
+            <Divider className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 mx-auto border-secondary-card" />
+            <SwitchDirectionButton className="z-10 p-2 rounded-full" />
+          </div>
+          <TokenSelectSection tokenDirection={TOKEN_DIRECTION.OUTPUT} />
         </div>
-        <TokenSelectSection tokenDirection={TOKEN_DIRECTION.OUTPUT} />
+        <SwapButton />
       </div>
-      <SwapButton />
+      <Disclaimer />
     </div>
   );
 };
