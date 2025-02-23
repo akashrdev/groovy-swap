7e84035f1f717e0096d22a556033ec2bbd00be76
akashrdev <akashrajan93@gmail.com>
2025-02-22 21:36:23 -0600

    Fix: Navbar z index

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -5,7 +5,7 @@ import { WalletConnect } from "../../wallet-connect";
 const ranchers = Ranchers({ subsets: ["latin"], weight: "400" });
 export const Navbar = () => {
   return (
-    <div className="w-screen border-b border-b-primary-border sticky left-0 right-0 top-0 h-14 px-6 py-2 justify-between flex items-center">
+    <div className="w-screen border-b border-b-primary-border sticky left-0 right-0 top-0 h-14 px-6 py-2 justify-between flex items-center z-10 bg-inherit">
       <span
         className={`${ranchers.className} sm:text-4xl text-primary-btn text-xl`}
       >
